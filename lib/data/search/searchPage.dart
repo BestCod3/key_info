b1f9682cc51ba59db6ac216fbd8a851ad856ab97
BestCod3 <erbol.kubatovv@mail.ru>
2023-06-11 20:28:30 +0600

    add configurations

--- lib/data/search/searchPage.dart
+++ lib/data/search/searchPage.dart
@@ -25,129 +25,155 @@ class SearchPageState extends State<SearchPage> {
   final List<ListItem> dataList = [
     ListItem(
       title: 'Кадыралиев А.Т.',
-      page: Rector1(),
+      page: const Rector1(),
     ),
     ListItem(
       title: 'Турсуналиева Динара Мухтаровна',
-      page: Rector2(),
+      page: const Rector2(),
     ),
     ListItem(
       title: 'Супаева Гулназ Тынаевна',
-      page: Rector3(),
+      page: const Rector3(),
     ),
     ListItem(
       title: 'Нурматова Айгуль Абдиллаевна',
-      page: Rector4(),
+      page: const Rector4(),
     ),
     ListItem(
       title: 'Нурматова Айгуль Абдиллаевна',
-      page: Rector4(),
+      page: const Rector4(),
     ),
     ListItem(
       title: 'Кафедра экономики и международных экономических отношений',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=9"),
+      page:
+          const Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=9"),
     ),
     ListItem(
       title: 'Кафедра финансы и финансовые технологии им. С.А.Сулайманбекова',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=34"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=34"),
     ),
     ListItem(
       title: 'Кафедра бухгалтерского учета, анализа и аудита',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=35"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=35"),
     ),
     ListItem(
       title:
           'Кафедра Туризм, гостеприимство и окружающая среда им. Чубуровой Ж.Т.',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=36"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=36"),
     ),
     ListItem(
       title: 'Цифровая экономика и программирование',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=37"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=37"),
     ),
     ListItem(
       title:
           'Кафедра товароведения, товарной экспертизы и ресторанного бизнеса',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=38"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=38"),
     ),
     ListItem(
       title: 'Кафедра экономико-математических методов и статистики',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=39"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=39"),
     ),
     ListItem(
       title: 'Кафедра государственного, официального и иностранных языков',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=40"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=40"),
     ),
     ListItem(
       title: 'Кафедра Прикладная экономика и менеджмент',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=41"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=41"),
     ),
     ListItem(
       title: 'Кафедра Международные финансы и экономическая безопасность',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=43"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=43"),
     ),
     ListItem(
       title: 'Кафедра Банковской деятельности и рынка ценных бумаг',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=42"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=42"),
     ),
     ListItem(
       title: 'Кафедра философии и социально-гуманитарных наук, бизнес и право',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=53"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=53"),
     ),
     ListItem(
       title: 'Центр физической культуры и спорта',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=134"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=134"),
     ),
     ListItem(
       title: 'Кафедра Коммерции и маркетинга',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=161"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=161"),
     ),
     ListItem(
       title: 'Кафедра Социальной работы',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=174"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=174"),
     ),
     ListItem(
       title: 'Учебно - методический отдел',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=50"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=50"),
     ),
     ListItem(
       title: 'Отдел кадров',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=51"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=51"),
     ),
     ListItem(
       title: 'Отдел качества и аккредитации',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=162"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=162"),
     ),
     ListItem(
       title: 'Отдел международного сотрудничества и коммуникации',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=52"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=52"),
     ),
     ListItem(
       title: 'Центр карьеры',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=54"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=54"),
     ),
     ListItem(
       title: 'Финансово - экономический отдел',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=55"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=55"),
     ),
     ListItem(
       title: 'Офис регистратор',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=62"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=62"),
     ),
     ListItem(
       title: 'Центр информационных технологий',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=56"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=56"),
     ),
     ListItem(
       title: 'Научно - методический ресурсный центр',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=57"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=57"),
     ),
     ListItem(
       title: 'Типография',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=58"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=58"),
     ),
     ListItem(
       title: 'Научная библиотека им. А. Орузбаева',
-      page: Kafedra(initialUrl: "http://www.keu.kg/site/menu-detail?mid=135"),
+      page: const Kafedra(
+          initialUrl: "http://www.keu.kg/site/menu-detail?mid=135"),
     ),
     // Добавьте остальные элементы списка с соответствующими страницами
   ];
@@ -166,16 +192,16 @@ class SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppbar(height: 90),
+      appBar: const CustomAppbar(height: 90),
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(8.0),
             child: TextField(
               onChanged: (value) {
                 search(value);
               },
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 hintText: 'Search',
               ),
             ),
@@ -200,7 +226,7 @@ class SearchPageState extends State<SearchPage> {
           ),
         ],
       ),
-      bottomNavigationBar: BottomBar(),
+      bottomNavigationBar: const BottomBar(),
     );
   }
 }
